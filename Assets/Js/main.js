ce38d5cec9b9eb2bc98e8e9d024920476bfde6a1
Aakash-git-code <aakashphotolab@gmail.com>
2024-09-11 17:16:03 +0530

    validation
    validation changes

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -184,16 +184,13 @@ function changeImage() {
   // validation form
   document.querySelector(".formSubmit").addEventListener("click", (e) => {
     e.preventDefault();
-    const username=document.getElementById("username").value;
     const phone=document.getElementById("phone").value;
     const email=document.getElementById("email").value;
-    const password=document.getElementById("password").value;
-    // const ConfirmPassword=document.getElementById("ConfirmPassword").value;
     
-    const usernameRegex=/^[A-Za-z0-9 ]{4,20}$/;
+  
     const phoneRegex=/^(\+\d{1,3}[- ]?)?\d{10}$/;
     const emailRegex=/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/;
-    const passwordRegex=/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d).*$/;
+
     
     //clear previous error
     document.querySelectorAll(".error")
@@ -201,11 +198,6 @@ function changeImage() {
     
     let isvalid=true;
     
-    //validate username
-    if(!usernameRegex.test(username)){
-        document.getElementById("usernameError").textContent="username is not valid";
-        isvalid=false;
-    }
     if(!phoneRegex.test(phone)){
         document.getElementById("phoneError").textContent="phone no. is not valid";
         isvalid=false;
@@ -214,16 +206,7 @@ function changeImage() {
         document.getElementById("emailError").textContent="please enter a valid email";
         isvalid=false;
     }
-    if(!passwordRegex.test(password)){
-        document.getElementById("passwordError").textContent="please enter a valid password";
-        isvalid=false;
-    }
-    
-    // if(!ConfirmPassword=== password){
-    //     document.getElementById("confirmpasswordError").textContent="please enter a same password";
-    //     isvalid=false;
-    // }
-    
+  
     
     
     let userData=[];
@@ -237,37 +220,11 @@ function changeImage() {
     
     });
     
-    // Name and Password from the register-form
-    var name = document.getElementById('username');
-    var pw = document.getElementById('password');
-    
     // storing input from register-form
     function store() {
         localStorage.setItem('username', username.value);
-        localStorage.setItem('password', password.value);
+        localStorage.setItem('phone', phone.value);
+        localStorage.setItem('email', email.value);
+        localStorage.setItem('product', product.value);
+        localStorage.setItem('msg', msg.value);
     }
-    
-    // check if stored data from register-form is equal to entered data in the   login-form
-    function check() {
-    
-        // stored data from the register-form
-        var storedusername = localStorage.getItem('username');
-        var storedpassword = localStorage.getItem('password');
-    
-        // entered data from the login-form
-        var username = document.getElementById('username');
-        var password = document.getElementById('password');
-    
-        // check if stored data from register-form is equal to data from login form
-        if(username.value !== storedusername || password.value !== storedpassword) {
-            alert('please enter correct userID and Password');
-        }else {
-            window.location.href = "profile.html";
-            alert('You are successfully loged in.');
-           
-        }
-    }
-    
-    function logout (){
-        window.location.href="login.html";
-      }
